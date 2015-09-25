118b426ec9124941073b08601aa0d2b23e3a9960
bens <5878794@qq.com>
2015-09-25 21:06:40 +0800

    add shock  h5 api

---
+++ js/lib/h5_api_shock.js
@@ -0,0 +1,34 @@
+/*
+ * Filename : 
+ * =====================================
+ * Created with WebStorm.
+ * User: bens
+ * Date: 15-9-25
+ * Time: 下午8:53
+ * Email:5878794@qq.com
+ * =====================================
+ * Desc:
+ */
+
+
+//开始震动
+//DEVICE.shock.start([30]);
+//opt:array    eg:[2000,1000,1000]  单位:ms
+//停止震动
+//DEVICE.shock.stop();
+
+
+//震动api h5  目前只支持android 4.4+
+DEVICE.shock = {
+	canUse:function(){
+		return  "vibrate" in navigator;
+	},
+	start:function(opt){
+		if(!this.canUse()){return;}
+		navigator.vibrate(opt);
+	},
+	stop:function(){
+		if(!this.canUse()){return;}
+		navigator.vibrate([]);
+	}
+};
