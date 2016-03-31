ef9fdb698a4c8d589caff7cd9379927659e50904
beens <5878794@qq.com>
2016-03-31 16:40:25 +0800

    add  viewport set

--- js/lib/viewport.js
+++ js/lib/viewport.js
@@ -23,11 +23,14 @@
         viewport = document.querySelector('meta[name="viewport"]'),
         dpr = window.devicePixelRatio || 1,
         scale = 1 / dpr,
-        rem = win_width/psd_width*100;
+        rem;
 
     viewport.setAttribute('content', 'width=' + dpr * win_width + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
 
     var style = document.createElement('style');
+    win_width = window.innerWidth;
+    rem = win_width/psd_width*100;
+
     style.innerHTML = "html{font-size:"+rem+"px!important;}";
     document.querySelector("head").appendChild(style);
 })();
