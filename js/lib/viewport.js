98a2ca87e05804da878b367a7fe3ec59c9ba081e
beens <5878794@qq.com>
2016-04-11 16:03:30 +0800

    add zs

--- js/lib/viewport.js
+++ js/lib/viewport.js
@@ -17,20 +17,21 @@
 //未考虑转屏的问题  TODO
 
 
-(function(){
-    var psd_width = 640,
-        win_width = window.innerWidth,
-        viewport = document.querySelector('meta[name="viewport"]'),
-        dpr = window.devicePixelRatio || 1,
-        scale = 1 / dpr,
-        rem;
 
-    viewport.setAttribute('content', 'width=' + dpr * win_width + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
-
-    var style = document.createElement('style');
-    win_width = window.innerWidth;
-    rem = win_width/psd_width*100;
-
-    style.innerHTML = "html{font-size:"+rem+"px!important;}";
-    document.querySelector("head").appendChild(style);
-})();
+//(function(){
+//    var psd_width = 640,
+//        win_width = window.innerWidth,
+//        viewport = document.querySelector('meta[name="viewport"]'),
+//        dpr = window.devicePixelRatio || 1,
+//        scale = 1 / dpr,
+//        rem;
+//
+//    viewport.setAttribute('content', 'width=' + dpr * win_width + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
+//
+//    var style = document.createElement('style');
+//    win_width = window.innerWidth;
+//    rem = win_width/psd_width*100;
+//
+//    style.innerHTML = "html{font-size:"+rem+"px!important;}";
+//    document.querySelector("head").appendChild(style);
+//})();
