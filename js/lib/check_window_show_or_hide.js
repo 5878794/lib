eac6e92cf511c24afea45250998802e5b3b276e1
beens <5878794@qq.com>
2016-06-24 14:34:59 +0800

    窗口隐藏在显示后触发函数执行

--- js/lib/check_window_show_or_hide.js
+++ js/lib/check_window_show_or_hide.js
@@ -5,6 +5,36 @@
 
 
 
+
+
+
+//窗口隐藏后显示时执行
+//DEVICE.windowShowRun(function(){
+//    console.log(123)
+//});
+
+(function(){
+    var isHiddened = false,
+        fn = function(){};
+    document.addEventListener('visibilitychange', function(e) {
+        if(document.hidden){
+            isHiddened = true;
+        }else{
+            if(isHiddened){
+                fn();
+            }
+        }
+    }, false);
+
+
+    DEVICE.windowShowRun = function(callback){
+        fn = callback || function(){};
+    };
+})();
+
+
+
+
 //判断当前窗口是否被隐藏  （最小化，切换tab会触发）
 
 //document.addEventListener('visibilitychange', function(e) {
