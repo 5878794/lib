77ef5dddc6bcb99c45740a63a27495965e80d572
beens <5878794@qq.com>
2016-06-30 16:26:59 +0800

    重力感应  接口

--- js/lib/gravitySensor-compiled.js
+++ js/lib/gravitySensor-compiled.js
@@ -6,9 +6,22 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 //重力感应
 
+//var obj = $("#test");
+//new DEVICE.gravitySensor({
+//    moveFn:function(x,y){
+//        //x:y轴旋转角度 -90 - 90
+//        //y:x轴旋转角度 -90 - 90
+//        //旋转角度可转换成百分比在转换成实际的移动像素x,y
+//        //手机横向时 x=y  y=x;
+//        obj.css3({transform:"translate3d("+x+"px,"+y+"px,0)"});
+//    }
+//});
+
 DEVICE.gravitySensor = function () {
     function sensor(opt) {
         _classCallCheck(this, sensor);
+
+        this.moveFn = opt.moveFn || function () {};
 
         this._init();
     }
@@ -21,13 +34,50 @@ DEVICE.gravitySensor = function () {
     }, {
         key: "_addEvent",
         value: function _addEvent() {
-            window.addEventListener("devicemotion", function (event) {
-                console.log(event);
-            }, true);
+            var _this = this;
+
+            window.addEventListener("deviceorientation", function (e) {
+                //设备水平放置到桌面时  beta和gamma的值都为0
+                //x:e.beta  -180 - 180   绕x轴旋转角度
+                //y:e.gamma  -90 - 90    绕y轴旋转角度
+
+                //e.alpha  0-360  设备指示的方向，根据指南针的设定情况而定
+                _this.handlerEvent(e);
+            }, false);
+        }
+    }, {
+        key: "handlerEvent",
+        value: function handlerEvent(e) {
+            var beta = e.beta,
+                gamma = e.gamma;
+
+            //强制只返回-90 - 90度的值
+            //beta = DEVICE.getBetweenNumber(beta,-90,90);
+            //gamma = DEVICE.getBetweenNumber(gamma,-90,90);
+
+            this._xMove(gamma, beta);
+        }
+    }, {
+        key: "_xMove",
+        value: function _xMove(x, y) {
+            y = DEVICE.getBetweenNumber(y, -90, 90);
+            //参数为度数
+            this.moveFn(x, y);
         }
     }]);
 
     return sensor;
 }();
+
+//$(document).ready(function(){
+//    var obj = $("#test");
+//    new DEVICE.gravitySensor({
+//        moveFn:function(x,y){
+//            console.log(y)
+//            obj.css3({transform:"translate3d("+x+"px,"+y+"px,0)"});
+////                console.log(x+"   "+y)
+//        }
+//    });
+//});
 
 //# sourceMappingURL=gravitySensor-compiled.js.map
