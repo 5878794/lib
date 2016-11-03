df8db8c544f61bfcbbc7f7d5d77e8e9e1d1b0309
beens <5878794@qq.com>
2016-11-03 15:17:41 +0800

    mdf  idcard  验证

--- js/lib/check_from-compiled.js
+++ js/lib/check_from-compiled.js
@@ -96,13 +96,30 @@
             return reg.test(str);
         },
         //身份证
-        idCard: function idCard(str) {
-            if (str.length == 0) {
+        idCard: function idCard(cid) {
+            if (cid.length == 0) {
                 return true;
             }
 
-            var reg = /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;
-            return reg.test(str);
+            var arrExp = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]; //加权因子
+            var arrValid = [1, 0, "X", 9, 8, 7, 6, 5, 4, 3, 2]; //校验码
+            if (/^\d{17}\d|x$/i.test(cid)) {
+                var sum = 0,
+                    idx;
+                for (var i = 0; i < cid.length - 1; i++) {
+                    // 对前17位数字与权值乘积求和
+                    sum += parseInt(cid.substr(i, 1), 10) * arrExp[i];
+                }
+                // 计算模（固定算法）
+                idx = sum % 11;
+                // 检验第18为是否与校验码相等
+                return arrValid[idx] == cid.substr(17, 1).toUpperCase();
+            } else {
+                return false;
+            }
+
+            //var reg = /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;
+            //return reg.test(str);
         },
         //中文
         chinese: function chinese(str) {
